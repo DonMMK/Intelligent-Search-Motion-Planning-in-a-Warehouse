807ad6f365cbb65c7f86ba9016e5f1872cf90288
don <misurakalu@gmail.com>
2022-04-23 19:28:25 +1000

    Updating the test functions to tests needed

--- assignment_1_code/sanity_check.py
+++ assignment_1_code/sanity_check.py
@@ -28,9 +28,20 @@ except ModuleNotFoundError:
     
 def test_taboo_cells():
     wh = Warehouse()
+
+    #NOTE: Text file - warehouse_01
+    #wh.load_warehouse("/Users/don/GitHub/Intelligent-Search-Motion-Planning-in-a-Warehouse/assignment_1_code/warehouses/warehouse_01.txt")
+    #expected_answer = '####  \n#X #  \n#  ###\n#   X#\n#   X#\n#XX###\n####  '
+
+    #NOTE: Text file - warehouse_09
+    #wh.load_warehouse("/Users/don/GitHub/Intelligent-Search-Motion-Planning-in-a-Warehouse/assignment_1_code/warehouses/warehouse_09.txt")
+    #expected_answer = '##### \n#  X##\n#X  X#\n##X  #\n ##X #\n  ## #\n   ###'
+
+    #NOTE: Text file - warehouse_03
     wh.load_warehouse("/Users/don/GitHub/Intelligent-Search-Motion-Planning-in-a-Warehouse/assignment_1_code/warehouses/warehouse_03.txt")
-    #expected_answer = '##### \n#. X##\n#X  X#\n##X  #\n ##X #\n  ##.#\n   ###' #NOTE: Text file - warehouse_09
-    expected_answer = '  ####   \n###  ####\n#       #\n# #  #  #\n# . .#  #\n#########' #NOTE: Text file - warehouse_03
+    expected_answer = '  ####   \n###XX####\n#X     X#\n#X#  # X#\n#X   #XX#\n#########' 
+
+
     answer = taboo_cells(wh)
     fcn = test_taboo_cells    
     print('<<  Testing {} >>'.format(fcn.__name__))
@@ -43,10 +54,10 @@ def test_taboo_cells():
         
 def test_check_elem_action_seq():
     wh = Warehouse()
-    wh.load_warehouse("/Users/don/GitHub/Intelligent-Search-Motion-Planning-in-a-Warehouse/assignment_1_code/warehouses/warehouse_09.txt")
+    wh.load_warehouse("/Users/don/GitHub/Intelligent-Search-Motion-Planning-in-a-Warehouse/assignment_1_code/warehouses/warehouse_01.txt")
     # first test
-    answer = check_elem_action_seq(wh, ['Right', 'Right','Down'])
-    expected_answer = '####  \n# .#  \n#  ###\n#*   #\n#  $@#\n#  ###\n####  '
+    answer = check_elem_action_seq(wh, ['Down', 'Right','Up'])
+    expected_answer = '####  \n# .#  \n#  ###\n#* @ #\n#   $#\n#  ###\n####  '
     print('<<  check_elem_action_seq, test 1>>')
     if answer==expected_answer:
         print('Test 1 passed!  :-)\n')
@@ -64,20 +75,49 @@ def test_check_elem_action_seq():
         print('Test 2 failed!  :-(\n')
         print('Expected ');print(expected_answer)
         print('But, received ');print(answer)
+    wh.load_warehouse("/Users/don/GitHub/Intelligent-Search-Motion-Planning-in-a-Warehouse/assignment_1_code/warehouses/warehouse_03.txt")
+    # third test
+    answer = check_elem_action_seq(wh, ['Right', 'Up','Up', 'Left', 'Left', 'Left', 'Up', 'Left', 'Down', 'Down'])
+    expected_answer = '  ####   \n###  ####\n#       #\n# #@ #$ #\n# .$.#  #\n#########' 
+    print('<<  check_elem_action_seq, test 1>>')
+    if answer==expected_answer:
+        print('Test 3 passed!  :-)\n')
+    else:
+        print('Test 3 failed!  :-(\n')
+        print('Expected ');print(expected_answer)
+        print('But, received ');print(answer)
+    # fourth test
+    answer = check_elem_action_seq(wh, ['Up', 'Right', 'Left', 'Down', 'Up'])
+    expected_answer = 'Impossible'
+    if answer==expected_answer:
+        print('Test 4 passed!  :-)\n')
+    else:
+        print('Test 4 failed!  :-(\n')
+        print('Expected ');print(expected_answer)
+        print('But, received ');print(answer)
 
 
 
 def test_solve_weighted_sokoban():
-    wh = Warehouse()    
-    wh.load_warehouse( "./warehouses/warehouse_8a.txt")
-    # first test
-    answer, cost = solve_weighted_sokoban(wh)
+    wh = Warehouse()
+    
+    
+    # NOTE: first test - Warehouse 8a
+    # wh.load_warehouse( "./warehouses/warehouse_8a.txt")
+    # answer, cost = solve_weighted_sokoban(wh)
 
-    expected_answer = ['Up', 'Left', 'Up', 'Left', 'Left', 'Down', 'Left', 
-                       'Down', 'Right', 'Right', 'Right', 'Up', 'Up', 'Left', 
-                       'Down', 'Right', 'Down', 'Left', 'Left', 'Right', 
-                       'Right', 'Right', 'Right', 'Right', 'Right', 'Right'] 
-    expected_cost = 431
+    # expected_answer = ['Up', 'Left', 'Up', 'Left', 'Left', 'Down', 'Left', 
+    #                    'Down', 'Right', 'Right', 'Right', 'Up', 'Up', 'Left', 
+    #                    'Down', 'Right', 'Down', 'Left', 'Left', 'Right', 
+    #                    'Right', 'Right', 'Right', 'Right', 'Right', 'Right'] 
+    # expected_cost = 431
+
+    # NOTE: second test - Warehouse 3
+    wh.load_warehouse( "/Users/don/GitHub/Intelligent-Search-Motion-Planning-in-a-Warehouse/assignment_1_code/warehouses/warehouse_03.txt")
+    answer, cost = solve_weighted_sokoban(wh) 
+    expected_answer = ['Up', 'Left'] 
+    expected_cost = 1
+
     print('<<  test_solve_weighted_sokoban >>')
     if answer==expected_answer:
         print(' Answer as expected!  :-)\n')
@@ -93,7 +133,7 @@ def test_solve_weighted_sokoban():
 
 if __name__ == "__main__":
     pass    
-#    print(my_team())  # should print your team
+    #print(my_team())  # should print your team
     test_taboo_cells() 
     #test_check_elem_action_seq()
     #test_solve_weighted_sokoban()
